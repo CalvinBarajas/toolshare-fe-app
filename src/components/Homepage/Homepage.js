746ecf5a3b9efd9433f2259d4b40a70171d997f5
Calvin Barajas <calvinbarajas@aim.com>
2022-07-16 16:10:35 -0700

    hyperlink every image on home page

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -2,6 +2,7 @@
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import { Link } from "react-router-dom";
 
 // images
 import Woodworking from "./img/woodworking.png";
@@ -22,54 +23,66 @@ const Homepage = () => {
         <Row className="mb-3">
           <Col sm={4}>
             WOODWORKING
-            <img
-              src={Woodworking}
-              alt="Woodworking"
-              className="d-block w-100 homeGridImg"
-            />
+            <Link to="/inventory">
+              <img
+                src={Woodworking}
+                alt="Woodworking"
+                className="d-block w-100 homeGridImg"
+              />
+            </Link>
           </Col>
           <Col sm={4}>
             PLUMBING
-            <img
-              src={Plumbing}
-              alt="Plumbing"
-              className="d-block w-100 homeGridImg"
-            />
+            <Link to="/inventory">
+              <img
+                src={Plumbing}
+                alt="Plumbing"
+                className="d-block w-100 homeGridImg"
+              />
+            </Link>
           </Col>
           <Col sm={4}>
             WELDING
-            <img
-              src={Welding}
-              alt="Welding"
-              className="d-block w-100 homeGridImg"
-            />
+            <Link to="/inventory">
+              <img
+                src={Welding}
+                alt="Welding"
+                className="d-block w-100 homeGridImg"
+              />
+            </Link>
           </Col>
         </Row>
         <Row>
           <Row>
             <Col sm={4}>
               ELECTRICAL
-              <img
-                src={Electrical}
-                alt="Electrical"
-                className="d-block w-100 homeGridImg"
-              />
+              <Link to="/inventory">
+                <img
+                  src={Electrical}
+                  alt="Electrical"
+                  className="d-block w-100 homeGridImg"
+                />
+              </Link>
             </Col>
             <Col sm={4}>
               AUTOMOTIVE
-              <img
-                src={Automotive}
-                alt="Automotive"
-                className="d-block w-100 homeGridImg"
-              />
+              <Link to="/inventory">
+                <img
+                  src={Automotive}
+                  alt="Automotive"
+                  className="d-block w-100 homeGridImg"
+                />
+              </Link>
             </Col>
             <Col sm={4}>
               GENERAL
-              <img
-                src={General}
-                alt="General"
-                className="d-block w-100 homeGridImg"
-              />
+              <Link to="/inventory">
+                <img
+                  src={General}
+                  alt="General"
+                  className="d-block w-100 homeGridImg"
+                />
+              </Link>
             </Col>
           </Row>
         </Row>
